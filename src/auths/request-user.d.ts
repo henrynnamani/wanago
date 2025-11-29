9ed61e98403f7489e10d8c37f69624a16e794ab6
gr4yx01 <hoyx0101@gmail.com>
2025-11-29 18:24:12 +0100

    restart

--- src/auths/request-user.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request } from '@nestjs/common';
-import { Response } from 'express';
-import { User } from 'src/users/user.entity';
-
-export interface RequestWithUser extends Request {
-  user: User;
-  res: Response;
-}
